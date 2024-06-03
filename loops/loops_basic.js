fdfa3950e9282942118b7055f0525e0a9f05fe9f
ZainBanyAli <banyalizain@gmail.com>
2024-06-04 00:56:47 +0300

    Update loops_basic.js

--- loops/loops_basic.js
+++ loops/loops_basic.js
@@ -1,59 +1,76 @@
 // 1- Write a program that prints numbers from 1 to 10 using a for loop.
 
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
+ for (let i = 1; i <= 10; i++) {
+  console.log(i);
+ }
 
 // 2-Write a program that prints the even numbers from 1 to 10 using a for loop.
 
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 == 0) {
-//     console.log(i);
-//   }
-// }
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
 
 // 3- Write a program that prints the odd numbers from 1 to 10 using a while loop.
 
-// let i = 1;
-// while (i <= 10) {
-//   if (i % 2 == 1) {
-//     console.log(i);
-//   }
-//   i++;
-// }
+ let i = 1;
+ while (i <= 10) {
+  if (i % 2 == 1) {
+     console.log(i);
+  }
+   i++;
+}
 // 4- Write a program that prints the multiplication table of a number entered by the user using a for loop.
 
-// var x = parseInt(prompt("Enter a number for x:"));
+ var x = parseInt(prompt("Enter a number for x:"));
 
-// for (let i = 0; i <= 10; i++) {
-//   var ans = i * x;
+ for (let i = 0; i <= 10; i++) {
+   var ans = i * x;
 
-//   console.log(ans);
-// }
+   console.log(ans);
+ }
 
 // 5- Write a program that calculates the sum of numbers from 1 to 100 using a while loop.
 
-// let i = 0;
-// let result = 0;
-// while (i <= 100) {
-//   result += i;
-//   i++;
-//   console.log(result);
-// }
+ let i = 0;
+ let result = 0;
+ while (i <= 100) {
+  result += i;
+   i++;
+   console.log(result);
+ }
 
 // 6- Write a program that calculates the factorial of a number entered by the user using a for loop.
 
-var x = parseInt(prompt("Enter a number for x:"));
+ var x = parseInt(prompt("Enter a number for x:"));
 
 let factorial = 1;
 for (let i = 1; i <= x; i++) {
   factorial *= i;
 }
 
-console.log(ans);
+console.log(factorial);
 
 // 7- Write a program that prints the Fibonacci series up to a certain number entered by the user using a while loop.
+var Z = parseInt(prompt("Enter a number for Z:"));
+
+let x = 0;
+let y = 1;
+while (x + y <= Z) {
+  var sum = x + y;
+  console.log(sum);
+  x = y;
+  y = sum;
+}
 
 // 8- Write a program that prints the reverse of the following numbers:
 //5 , 10 , 15 , 20
 // using a while loop.
+
+var numbers = [5, 10, 15, 20];
+var i = numbers.length - 1;
+while (i >= 0) {
+  console.log(numbers[i]);
+  i--;
+}
